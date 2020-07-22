b5d1885d214931db02f1022163cb35a7dcae35d4
dz-s <shulhindvst@gmail.com>
2020-07-22 23:43:34 +0200

    upd

--- problem2.cpp
+++ problem2.cpp
@@ -8,21 +8,27 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    for(int a0 = 0; a0 < t; a0++){
+    for (int a0 = 0; a0 < t; a0++)
+    {
         long n;
         cin >> n;
-        unsigned long int t1=1,t2=2,next_term=0;
-        long sum=0;
-        for(;t1<=n;){
-            if((t1%2)==0){ sum+=t1; }
-            next_term=t1+t2;
-            t1=t2;
-            t2=next_term;
+        unsigned long int t1 = 1, t2 = 2, next_term = 0;
+        long sum = 0;
+        for (; t1 <= n;)
+        {
+            if ((t1 % 2) == 0)
+            {
+                sum += t1;
+            }
+            next_term = t1 + t2;
+            t1 = t2;
+            t2 = next_term;
         }
-        cout<<sum<<endl;
+        cout << sum << endl;
     }
     return 0;
 }
