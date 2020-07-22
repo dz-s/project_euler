eaca74adebd7e510fad42dbd7384b5a9e44c8248
dz-s <shulhindvst@gmail.com>
2020-07-22 23:36:00 +0200

    upd

---
+++ problem2.cpp
@@ -0,0 +1,28 @@
+//Accepted
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        long n;
+        cin >> n;
+        unsigned long int t1=1,t2=2,next_term=0;
+        long sum=0;
+        for(;t1<=n;){
+            if((t1%2)==0){ sum+=t1; }
+            next_term=t1+t2;
+            t1=t2;
+            t2=next_term;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
